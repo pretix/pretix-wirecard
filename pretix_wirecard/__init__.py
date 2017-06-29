e8aad5d561156814bcc785e2291c275cb51037b7
Raphael Michel <mail@raphaelmichel.de>
2017-06-29 12:12:53 +0200

    Initial commit

---
+++ pretix_wirecard/__init__.py
@@ -0,0 +1,19 @@
+from django.apps import AppConfig
+
+
+class PluginApp(AppConfig):
+    name = 'pretix_wirecard'
+    verbose_name = 'pretix Wirecard integration'
+
+    class PretixPluginMeta:
+        name = 'pretix Wirecard integration'
+        author = 'Raphael Michel'
+        description = 'This plugin integrates Wirecard payment methods with pretix'
+        visible = True
+        version = '1.0.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretix_wirecard.PluginApp'
